68a5a61fef6b98bced21966ac49592aadbb3dd88
Manu Rink <manuela.schrenk@gmail.com>
2016-06-08 12:59:01 +0200

    initial app with hockey integration in place

---
+++ MyHockeyTestApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyHockeyTestApp
+//
+//  Created by Manu Rink on 07/06/16.
+//  Copyright © 2016 Manu Rink. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
